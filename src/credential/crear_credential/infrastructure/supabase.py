f78edb6065b0f093f23cbd6c0bfc9da90318d805
Morales1014 <moralessantiago20034@gmail.com>
2025-04-20 15:34:46 -0500

    agentes completos

--- src/credential/crear_credential/infrastructure/supabase.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client
-
-class SupabaseCrearCredential:
-    def __init__(self, url: str, key: str) -> None:
-        self.client = create_client(url, key)
-
-    def CrearCredentialConnect(self):
-        # Retorna la tabla "credential" de Supabase
-        return self.client.table("credential")
